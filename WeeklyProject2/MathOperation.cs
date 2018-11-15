0243e1a2f239d3e8e60cf9a9168ad1194ee7face
Giorgos Kalomalos <Giorgos.Kalomalos@Peoplecert.onmicrosoft.com>
2018-11-15 13:22:29 +0200

    changed static mathOperation to void DoCalculations

--- WeeklyProject2/MathOperation.cs
+++ WeeklyProject2/MathOperation.cs
@@ -8,26 +8,35 @@ namespace WeeklyProject2
 {
     class MathOperation
     {
-        static void SymbolsToList()
-        {
-            List<string> Symbols = new List<string>();
-            Symbols.Add("+");
-            Symbols.Add("-");
-            Symbols.Add("*");
-            Symbols.Add("/");
-            Symbols.Add("%");
-            Symbols.Add("square root");
-        }
+        //static void SymbolsToList()
+        //{
+        //    List<string> Symbols = new List<string>();
+        //    Symbols.Add("+");
+        //    Symbols.Add("-");
+        //    Symbols.Add("*");
+        //    Symbols.Add("/");
+        //    Symbols.Add("%");
+        //    Symbols.Add("square root");
+        //}
         
 
+        //public int Num1 { get; set; }
+        //public int Num2 { get; set; }
 
-        public int Num1 { get; set; }
-        public int Num2 { get; set; }
-
-        MathOperation(int num1, int num2)
+        public void DoCalculations()
         {
-            num1 = Num1;
-            num2 = Num2;
+            int num1, num2;
+
+            Console.WriteLine("Type a number and press Enter : ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nType a second number and press Enter : ");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Choose a math operation between these two numbers.\nChoices are + , - , * , / , % , square root");
+            string mathOperation = Console.ReadLine();
+
+
+            //num1 = Num1;
+            //num2 = Num2;
 
             switch (mathOperation)
             {
--- WeeklyProject2/Program.cs
+++ WeeklyProject2/Program.cs
@@ -10,55 +10,16 @@ namespace WeeklyProject2
     {
         static void Main(string[] args)
         {
-            int num1 = 0; int num2 = 0;
-            string mathOperation;
-            int result;
+
+            MathOperation math = new MathOperation();
+            math.DoCalculations();
+
 
             
-
-            Console.WriteLine("Type a number and press Enter : ");
-            num1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("\nType a second number and press Enter : ");
-            num2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Choose a math operation between these two numbers.\nChoices are + , - , * , / , % , square root");
-            mathOperation = Console.ReadLine();
-
-            switch (mathOperation)
-            {
-                case "+":
-                    result = num1 + num2;
-                    Console.WriteLine($"Result : {num1} + {num2} = " + (num1 + num2));
-                    break;
-                case "-":
-                    result = num1 + num2;
-                    Console.WriteLine($"Result : {num1} - {num2} = " + (num1 - num2));
-                    break;
-                case "*":
-                    result = num1 + num2;
-                    Console.WriteLine($"Result : {num1} * {num2} = " + (num1 * num2));
-                    break;
-                case "/":
-                    Console.WriteLine($"Result : {num1} / {num2} = " + (num1 / num2));
-                    result = num1 + num2;
-                    break;
-                case "%":
-                    Console.WriteLine($"Result : {num1} % {num2} = " + (num1 % num2));
-                    result = num1 + num2;
-                    break;
-                case "square root":
-                    Console.WriteLine($"Result : {num1} sqrt {num2} = " + (Math.Pow(num1, (1/num2))));
-                    //TODO check division by zero
-                    break;
-                default:
-                    Console.WriteLine("Incorrect Option");
-                    break;
-
-            }
 
             Console.WriteLine("Press any key to close the app");
             Console.ReadKey();
 
         }
-
     }
 }
